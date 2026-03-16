1cf4713584f0be021b659268ec4ba771fe586fa5
darmar12 <dima-o@mail.ru>
2026-03-16 16:38:17 +0300

    first step

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,8 @@
+import { useParams } from 'react-router-dom';
+import { products } from '../data/data';
+
+function ProductDetails() {
+  return <div></div>;
+}
+
+export default ProductDetails;
---
+++ src/pages/Thanks.jsx
@@ -0,0 +1,8 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Thanks() {
+  return <div>You will be redirected to the home page in 5 seconds...</div>;
+}
+
+export default Thanks;
