155ff7c2e310445dcf8e62d333985afb05096323
darmar12 <dima-o@mail.ru>
2026-03-17 15:55:11 +0300

    final

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,24 +1,48 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
 import { products } from '../data/data';
+import NotFound from './NotFound';
 
 function Category() {
   const currentCategoryId = useParams().id;
-  const productList = products.filter((product) => product.categoryId === currentCategoryId);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const maxPrice = Number(searchParams.get('maxPrice')) || Infinity;
+  const productList = products.filter(
+    (product) => product.categoryId === currentCategoryId && product.price <= maxPrice,
+  );
 
-  console.log(productList);
+  function handleMaxPrice(e) {
+    const value = e.target.value;
+    setSearchParams(value ? { maxPrice: value } : {});
+  }
 
   return (
     <div>
-      <ul style={{ display: 'flex' }}>
-        {productList.map((product) => (
-          <li key={product.id}>
-            <Link to={`/product/${product.id}`}>
-              {product.name} ${product.price}
-              <img src={product.img} alt={product.name} style={{ width: '150px' }} />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1>Category - {currentCategoryId}</h1>
+      <label htmlFor="maxPrice">
+        <input
+          type="number"
+          id="maxPrice"
+          value={searchParams.get('maxPrice') || ''}
+          placeholder="Enter maxprice"
+          onChange={handleMaxPrice}
+        />
+      </label>
+      {productList.length > 0 ? (
+        <>
+          <ul style={{ display: 'flex' }}>
+            {productList.map((product) => (
+              <li key={product.id}>
+                <Link to={`/product/${product.id}`}>
+                  {product.name} ${product.price}
+                  <img src={product.img} alt={product.name} style={{ width: '150px' }} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <NotFound />
+      )}
     </div>
   );
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,30 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { products } from '../data/data';
+import NotFound from './NotFound';
 
 function ProductDetails() {
-  return <div></div>;
+  const currentProductId = useParams().id;
+  const product = products.find((product) => product.id === parseInt(currentProductId));
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'baseline', gap: '20px' }}>
+      {product ? (
+        <>
+          <h2>{product.name}</h2>
+          <p>Price: ${product.price}</p>
+          <p>
+            Category: <Link to={`/category/${product.categoryId}`}>{product.categoryId}</Link>
+          </p>
+          <img src={product.img} alt={product.name} style={{ width: '150px' }} />
+          <button type="button" className="btn">
+            Buy
+          </button>
+        </>
+      ) : (
+        <NotFound />
+      )}
+    </div>
+  );
 }
 
 export default ProductDetails;
